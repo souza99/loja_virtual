ba468049112af063831d2bedcaa359f3a454e228
souza99 <jpfsouza99@gmail.com>
2022-05-20 21:10:35 -0300

    Iniciando projeto

---
+++ loja/src/main/java/com/loja_virtual/loja/com.loja_virtual.domain/LojaDomain.java
@@ -0,0 +1,29 @@
+package com.loja_virtual.loja.domain;
+
+
+@Entity
+@Table(name = produto)
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Getter
+@Setter
+
+public class LojaDomain implements Serializable{
+
+    private static final long serialVersionUID = 4048798961366546485L;
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private id;
+
+
+    @Column(name="nome")
+    private nome;
+
+    @Column(name="preco")
+    private preco;
+
+    @Column(name="descricao")
+    private descricao;
+
+}
